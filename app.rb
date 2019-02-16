042312fb6875b4e0d53317d240f32691022cf967
Jessica Gladstone <jcg321@comcast.net>
2019-02-16 16:30:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session_secret, "xenacali"
+  end
+  
+  get '/' do
+    @session = session
+  end
+  
+  get '/' do
+    erb :index
+  end
+  
+  post '/checkout' do
+    
+  end
 end
