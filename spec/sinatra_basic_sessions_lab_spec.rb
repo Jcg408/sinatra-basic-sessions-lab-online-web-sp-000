76ad92661ad9e738f32c0d23baa380c5086bc4a4
Jessica Gladstone <jcg321@comcast.net>
2019-02-16 16:37:26 +0000

    Automatically backed up by Learn

--- spec/sinatra_basic_sessions_lab_spec.rb
+++ spec/sinatra_basic_sessions_lab_spec.rb
@@ -7,8 +7,8 @@ describe App do
       expect(last_response.status).to eq(200)
     end
 
-    # it 'renders a form to purchase an item' do 
-    #   visit '/'
+     it 'renders a form to purchase an item' do 
+       visit '/'
     #   expect(page).to have_selector("form")
       expect(page).to have_field(:item)
     end
